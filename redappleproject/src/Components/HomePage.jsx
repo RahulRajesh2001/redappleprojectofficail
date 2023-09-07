3961df7ca4322909eccae78a4670db321493398c
RahulRajesh2001 <rahulrajesh2001239@gmail.com>
2023-09-07 14:49:24 +0530

    changes

--- redappleproject/src/Components/HomePage.jsx
+++ redappleproject/src/Components/HomePage.jsx
@@ -7,14 +7,14 @@ const HomePage = () => {
         <div className='w-full h-screen flex relative justify-center items-center'>
           <div className="bg-[url('../public/images/Homepageimage.png')] w-full h-screen absolute z-10"></div>
           <div
-            className='flex flex-col justify-center items-center text-[#ffff]  absolute z-20 font-Lora  font-normal
-           vsm:text-[32px] vsm:w-[300px] vsm:h-[150px] 
-           sm:text-[45px] sm:w-[500px] sm:h-[150px]
-           md:w-[650px] md:h-[200px] md:text-[65px]
-           lg:w-[700px] lg:h-[250px] lg:text-[65px]'
+            className='flex flex-col justify-center items-center   absolute z-20   
+            vsm:w-[300px] vsm:h-[150px] 
+           sm:w-[500px] sm:h-[150px]
+           md:w-[650px] md:h-[200px] 
+           lg:w-[700px] lg:h-[250px] '
           >
-            <p>Your One-Stop Shop</p>
-            <p>for Wholesale Fashion</p>
+            <p className='font-Lora font-normal text-[#ffff] vsm:text-[32px]  sm:text-[45px] md:text-[65px] lg:text-[65px]'>Your One-Stop Shop</p>
+            <p className='font-Lora font-normal text-[#ffff] vsm:text-[32px]  sm:text-[45px] md:text-[65px] lg:text-[65px]'>for Wholesale Fashion</p>
           </div>
         </div>
       </div>
